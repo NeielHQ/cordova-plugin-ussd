bb72ad00491be0233f7eeb2d4e04ef5e7c041b8c
Donald <donald@neiel.ng>
2021-06-06 11:46:47 +0100

    Changed plugin init method

--- www/ussd.js
+++ www/ussd.js
@@ -1,5 +1,11 @@
-window.ussd = function(str, callback) {
-    cordova.exec(callback, function(err) {
-        callback('Nothing to echo.');
-    }, "ussd", "dial", [str]);
+// window.ussd = function(str, callback) {
+//     cordova.exec(callback, function(err) {
+//         callback('Nothing to echo.');
+//     }, "ussd", "dial", [str]);
+// };
+
+var exec = require('cordova/exec');
+
+exports.ussd = function (str, success, error) {
+    exec(success, error, 'ussd', 'dial', [str])
 };
