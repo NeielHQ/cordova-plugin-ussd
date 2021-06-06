47d09a9c86507779b785a0e8d2e03a14f59ac3b6
Donald <donald@neiel.ng>
2021-06-06 22:37:05 +0100

    Experimenting with JS

--- www/ussd.js
+++ www/ussd.js
@@ -1,37 +1,7 @@
-// window.ussd = function(str, callback) {
-//     cordova.exec(callback, function(err) {
-//         callback('Nothing to echo.');
-//     }, "ussd", "dial", [str]);
-// };
-// cordova.define
-// var exec = require('cordova/exec');
-//
-// module.exports={
-//     ussd : function (str, success, error) {
-//         exec(success, error, 'ussd', 'dial', [str])
-//     }
-// }
+/*global cordova, module*/
 
-
-// Empty constructor
-function USSDPlugin() {}
-
-// The function that passes work along to native shells
-// Message is a string, duration may be 'long' or 'short'
-USSDPlugin.prototype.dial = function(str, successCallback, errorCallback) {
-    // var options = {};
-    // options.ussdCode = ussdCode;
-    // options.sim = sim;
-
-    cordova.exec(successCallback, errorCallback, 'ussd', 'dial', [str]);
-}
-
-// Installation constructor that binds ToastyPlugin to window
-USSDPlugin.install = function() {
-    if (!window.plugins) {
-        window.plugins = {};
+module.exports = {
+    dial: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Ussd", "dial", [name]);
     }
-    window.plugins.ussd= new USSDPlugin();
-    return window.plugins.ussd;
 };
-cordova.addConstructor(USSDPlugin.install);
