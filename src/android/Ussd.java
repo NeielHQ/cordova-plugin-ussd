5cf8495fe03c261418bece05f1810bf821153164
Donald <donald@neiel.ng>
2021-06-06 23:06:25 +0100

    Added intents and Java relephony code

--- src/android/Ussd.java
+++ src/android/Ussd.java
@@ -1,8 +1,19 @@
-package ng.neiel.cordova.plugins.Ussd;
+package ng.neiel.cordova.plugins;
 
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.List;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.telephony.TelephonyManager;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import android.os.Handler;
 
 public class Ussd extends CordovaPlugin {
 
@@ -11,9 +22,38 @@ public class Ussd extends CordovaPlugin {
 
         if (action.equals("dial")) {
 
-            String name = data.getString(0);
-            String message = "Hello, " + name;
-            callbackContext.success(message);
+            String code = data.getString(0);
+//             String name = data.getString(0);
+//             String message = "Hello, " + name;
+
+          TelephonyManager manager = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+          TelephonyManager manager2 = manager.createForSubscriptionId(2);
+
+          TelephonyManager managerMain = (sim == 0) ? manager : manager2;
+
+          managerMain.sendUssdRequest(ussdCode, new TelephonyManager.UssdResponseCallback() {
+              @Override
+              public void onReceiveUssdResponse(TelephonyManager telephonyManager, String request, CharSequence response) {
+                  super.onReceiveUssdResponse(telephonyManager, request, response);
+
+                  Log.e("TAG", "onReceiveUssdResponse:  Ussd Response = " + response.toString().trim() );
+                    callbackContext.success(new PluginResult(PluginResult.Status.OK, response.toString().trim()));
+
+
+
+
+              }
+
+              @Override
+              public void onReceiveUssdResponseFailed(TelephonyManager telephonyManager, String request, int failureCode) {
+                  super.onReceiveUssdResponseFailed(telephonyManager, request, failureCode);
+
+                  Log.e("TAG", "onReceiveUssdResponseFailed: " + "" + failureCode + request);
+                  callbackContext.error(new PluginResult(PluginResult.Status.OK, "We Bad"));
+
+              }
+          }, new Handler());
+//             callbackContext.success(message);
 
             return true;
 
